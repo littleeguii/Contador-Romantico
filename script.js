1d15e54e30569cc5cc451734c45a310c825b391a
Guilherme Santos Nunes <139825386+littleeguii@users.noreply.github.com>
2026-02-20 15:52:09 -0300

    script.js

--- script.js
+++ script.js
@@ -1,11 +1,7 @@
-// --- para configurar o contador ---
 
-// Coloque a data de início do namoro aqui
-// Formato: Ano, Mês (começa do 0!), Dia
-// (Janeiro é 0, Fevereiro é 1, Março é 2, etc...)
-const dataInicio = new Date(2024, 11, 21); // <<< MUDE AQUI (Ex: 15 de Jan de 2023)
+const dataInicio = new Date(2024, 11, 21);
 
-// onde o numero vai ser exibido
+
 const elementoContador = document.getElementById('dias-contador');
 
 function atualizarContador() {
@@ -15,28 +11,24 @@ function atualizarContador() {
     elementoContador.textContent = diasPassados;
 }
 
-// --- vamos por a música agora) ---
-
-// Pega os elementos de áudio e o botão
 const musica = document.getElementById('musica-fundo');
 const botaoMusica = document.getElementById('musica-toggle-btn');
 
-//define o volume
-// (0.0 = mudo, 1.0 = máximo. 0.5 é 50% do volume)
+
 musica.volume = 0.5;
 
-// play/pause
+
 botaoMusica.addEventListener('click', () => {
-    // Verifica se a música está pausada (ou não começou)
+
     if (musica.paused) {
-        musica.play(); // Toca a música
-        botaoMusica.textContent = 'Pausar ⏸️'; // Muda o texto do botão
+        musica.play();
+        botaoMusica.textContent = 'Pausar';
     } else {
-        musica.pause(); // Pausa a música
-        botaoMusica.textContent = 'Tocar 🎵'; // Muda o texto de volta
+        musica.pause();
+        botaoMusica.textContent = 'Tocar';
     }
 });
 
 
-// Chama a função de atualizar o contador assim que a página carregar
+
 document.addEventListener('DOMContentLoaded', atualizarContador);
